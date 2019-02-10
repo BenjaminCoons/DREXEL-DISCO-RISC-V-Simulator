69bc859cd5b2acdf63f8b97355b5a5565f9284cd
Adam Bengis <itbengis@gmail.com>
2019-02-09 23:14:52 -0500

    Prototype processor funblocks

--- src/single_cycle/Core.h
+++ src/single_cycle/Core.h
@@ -33,22 +33,40 @@ private:
 
 	unsigned long long int clk;
 
-	/*
-		Group One: Design Components Here, an instruction memory has already been
-		designed for you.
-	*/
 	long PC;
 
 	Instruction_Memory *instr_mem;
 
-	/*
-		Group Two: Simulator Related
-	*/
 	list<Instruction> pending_queue;
 
 	void serve_pending_instrs();
 
 	void printStats(list<Instruction>::iterator &ite);
+
+	uint64_t add64(uint64_t a, uint64_t b);
+
+	uint64_t mux64(uint64_t a, uint64_t b, bool s);
+
+	uint64_t lshift64(uint64_t a, uint8_t s = 1);
+
+	bool and_gate(bool a, bool b);
+
+	void control(uint8_t instr, bool *branch, bool *memread, bool *memtoreg,
+		uint8_t *aluop, bool *memwrite, bool *alusource, bool *regwrite);
+
+	uint64_t immgen(uint32_t instr);
+
+	void imem(uint32_t addr, uint8_t *control, uint8_t *read1, uint8_t *read2, 
+		uint8_t *write, uint32_t *immgen);
+
+	uint8_t alu_control(uint8_t aluop, uint8_t func3, uint8_t func7);
+
+	uint64_t alu(uint64_t a, uint64_t b, uint8_t control, bool *zero);
+
+	void reg(uint8_t reg1, uint8_t reg2, uint8_t wreg, uint64_t wdata, 
+		uint64_t *data1, uint64_t *data2);
+
+	uint64_t datmem(uint64_t addr, uint64_t wdata, bool read, bool write);
 };
 
 #endif
